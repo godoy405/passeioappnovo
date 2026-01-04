be15cd9f3fc44db432b5e343e8e290ddab347619
Gilberto <gilbertonerigodoy@gmail.com>
2026-01-04 19:55:59 -0300

    Criação do formulário parte 2

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+  camposForm: FormGroup;
+
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required)
+    });
+  }
+
+  salvar() {
+    console.log('valores digitados:', this.camposForm.value);
+    console.log('Está valido ?', this.camposForm.valid);
+    }
 
 }
