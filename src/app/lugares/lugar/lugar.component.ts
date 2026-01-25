e95baba8144682454a9ae3330cd2b62e7ae96159
Gilberto <gilbertonerigodoy@gmail.com>
2026-01-24 22:17:55 -0300

    Criação da parte 1 da galeria

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,29 +1,59 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../lugar.service';  // ← ADICIONAR ESTA LINHA
 
 @Component({
   selector: 'app-lugar',
   standalone: false,
+
   templateUrl: './lugar.component.html',
   styleUrl: './lugar.component.scss'
 })
-export class LugarComponent {
+export class LugarComponent implements OnInit {
+
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor() {
+  constructor(
+    private categoriaService: CategoriaService,
+    private service: LugarService
+
+  ){
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
       localizacao: new FormControl('', Validators.required),
       urlFoto: new FormControl('', Validators.required),
       avaliacao: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe({
+      next: (listaCategorias) => this.categorias = listaCategorias
     })
   }
 
   salvar(){
-    console.log("valores: ", this.camposForm.value);
+    this.camposForm.markAllAsTouched
+
+    if(this.camposForm.valid){
+      this.service.salvar(this.camposForm.value)
+        .subscribe({
+          next: (lugar) => {
+            console.log('Lugar salvo com sucesso', lugar)
+            this.camposForm.reset();
+          },
+          error: (erro) => console.error('Erro ao salvar lugar', erro)
+        });
+
+      }
   }
 
+  isCampoInvalido(nomeCampo: string): boolean {
+      const campo = this.camposForm.get(nomeCampo);
+      return campo?.invalid && campo?.touched && campo?.errors?.['required'];
+    }
 }
