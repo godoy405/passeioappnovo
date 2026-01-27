2c3156deed443537bdfc884afe8dbdd43cda3478
Gilberto <gilbertonerigodoy@gmail.com>
2026-01-27 19:03:24 -0300

    Finalização da parte Galeria

--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Lugar } from './lugar';
 import { Observable } from 'rxjs';
 
@@ -13,7 +13,29 @@ export class LugarService {
   salvar(lugar: Lugar) : Observable<Lugar>{
     return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
   }
+
   obterTodos() : Observable<Lugar[]> {
     return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
+
+  filtrar(nome: string, categoria: string) : Observable<Lugar[]> {
+
+    let parametros = new HttpParams();
+
+    if(nome){
+      parametros = parametros.set('nome_like', nome);
+
+      }
+
+      if(categoria && categoria !== '-1'){
+      parametros = parametros.set('categoria', categoria);
+
+      }
+
+    // http://localhost:3000/lugares?nome_like=parque&categoria=parque
+
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares', {
+      params: parametros
+    });
   }
 }
